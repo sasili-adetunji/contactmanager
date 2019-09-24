3bf5612d24cdb8f37143c353f5cac514ba09ce52
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-09-24 22:03:30 +0100

    adds bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager"/>
-      <Contact name="John Doe" email="jdoe@gmail.com" phone="08037817325" />
-      <Contact name="Karen Smith" email="ksmith@gmail.com" phone="090372873482" />
+      <div className="container">
+        <Contact
+          name="John Doe"
+          email="jdoe@gmail.com"
+          phone="08037817325" />
+        <Contact
+          name="Karen Smith"
+          email="ksmith@gmail.com"
+          phone="090372873482" />
+      </div>
     </div>
   );
 }
