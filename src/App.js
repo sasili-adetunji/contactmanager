8660b101e00bbe6799d2d7b4efae4fb1165a417f
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-09-25 14:51:06 +0100

    add forms

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import './App.css';
-import Contacts from './components/Contacts';
-import Header from './components/Header';
+import Contacts from './components/contact/Contacts';
+import Header from './components/layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Provider } from './context';
-
+import AddContact from './components/contact/AddContact';
 function App() {
   return (
     <Provider>
       <div className="App">
         <Header branding="Contact Manager"/>
         <div className="container">
+          <AddContact />
           <Contacts />
         </div>
       </div>
