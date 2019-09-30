91f8b682220a6617456b680af5efaea728129e2f
Sasiliyu Adetunji <sasil.adetunji@gmail.com>
2019-09-30 12:24:39 +0100

    Updates

---
+++ precache-manifest.409062c859995e127e195c3338792a61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2114f8219841ca951630edcdd529ad99",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "d0ee25c5a109b315ea2b",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "be207af6e5045ed69ea0",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "d0ee25c5a109b315ea2b",
+    "url": "/contactmanager/static/js/2.7199b02c.chunk.js"
+  },
+  {
+    "revision": "be207af6e5045ed69ea0",
+    "url": "/contactmanager/static/js/main.db024b26.chunk.js"
+  },
+  {
+    "revision": "1fc28d4712163ccd2b22",
+    "url": "/contactmanager/static/js/runtime-main.68e2db06.js"
+  }
+]);
